826dc0bff83ee359bd8c47da92dd6433cd7d4ab3
Yuxuan Hu <huyuxuan@IITS-B473-02.utsc-labs.utoronto.ca>
2018-04-01 22:42:38 -0400

    update

--- server/server.js
+++ server/server.js
@@ -42,14 +42,14 @@ app.use(flash());
 app.use(function (req, res, next) {
   console.log('starting server ..')
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://drawthree.herokuapp.com:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*drawthree.herokuapp.com:3000');
   // res.setHeader('Access-Control-Allow-Origin', 'https://drawsquad.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
